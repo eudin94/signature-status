7e18c0ca87efa5e34847527cb85054e5c26d687e
Eduardo.Comerlato <eduardo.comerlato@southsystem.com.br>
2022-03-06 17:02:04 -0300

    demo project impl

---
+++ src/main/java/com/comerlato/signature_status/SignatureStatusApplication.java
@@ -0,0 +1,13 @@
+package com.comerlato.signature_status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatureStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comerlato/signature_status/SignatureStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comerlato.signature_status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
