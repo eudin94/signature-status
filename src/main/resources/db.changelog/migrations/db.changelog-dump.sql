fe2e35dcce7b9d4ebc0a2e83358ad14be2757a5e
Eduardo.Comerlato <eduardo.comerlato@southsystem.com.br>
2022-03-07 17:16:44 -0300

    feeding changelog dump

--- src/main/resources/db.changelog/migrations/db.changelog-dump.sql
+++ src/main/resources/db.changelog/migrations/db.changelog-dump.sql
@@ -1,1 +1,30 @@
 --liquibase formatted sql
+--changeset eduardo.comerlato:1
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f01', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:2
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f02', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:3
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f03', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:4
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f04', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:5
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f05', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:6
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f06', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:7
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f07', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:8
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f08', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:9
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f09', CURRENT_TIMESTAMP);
+
+--changeset eduardo.comerlato:10
+INSERT INTO subscription (id, created_at) VALUES ('5793cf6b3fd833521db8c420955e6f00', CURRENT_TIMESTAMP);
