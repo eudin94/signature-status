3ff8dc5e34ba15285e745d89e757a9dd97522cde
Eduardo.Comerlato <eduardo.comerlato@southsystem.com.br>
2022-03-07 12:29:36 -0300

    status controller impl

--- src/main/java/com/comerlato/signature_status/resource/StatusResource.java
+++ src/main/java/com/comerlato/signature_status/resource/StatusResource.java
@@ -1,10 +1,20 @@
 package com.comerlato.signature_status.resource;
 
+import com.comerlato.signature_status.dto.StatusDTO;
+import com.comerlato.signature_status.enums.StatusEnum;
 import com.comerlato.signature_status.service.StatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/status")
@@ -14,4 +24,21 @@ public class StatusResource {
 
     private final StatusService service;
 
+    @GetMapping
+    @ResponseStatus(OK)
+    @Operation(summary = "Find all status", responses = {@ApiResponse(responseCode = "200")})
+    public Page<StatusDTO> findAll(@RequestParam(required = false) final Optional<StatusEnum> name,
+                                   @RequestParam(required = false) final Integer page,
+                                   @RequestParam(required = false) final Integer size,
+                                   @RequestParam(required = false) final String sort,
+                                   @RequestParam(required = false) final Sort.Direction direction) {
+        return service.findAll(name, PageRequest.of(page, size, Sort.by(direction, sort)));
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(OK)
+    @Operation(summary = "Find all status", responses = {@ApiResponse(responseCode = "200")})
+    public StatusDTO findDTOById(@PathVariable final Long id) {
+        return service.findDTOByID(id);
+    }
 }
